5bf3e6e78c7e86d736f872127267644be32ec7c4
gio <you@example.com>
2021-12-30 20:10:00 +0200

    added tooltip on share button using tippy.js for react

--- src/components/userpage/secondColumn/Post.js
+++ src/components/userpage/secondColumn/Post.js
@@ -1,5 +1,7 @@
+import Tippy from '@tippyjs/react'
 import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
+
 
 export default function Post(props) {
 
@@ -19,7 +21,7 @@ export default function Post(props) {
         <div id={props.id} className='post-container'>
             <Card className='panel panel-default' >
                 <Card.Body className='feed-post-container'>
-                    <Card.Img variant="top" src={props.image} style={postStyles[1]}/>
+                    <Card.Img variant="top" src={props.image} style={postStyles[1]} />
                     <Card.Title className="feed-title"><h4>{props.date}</h4></Card.Title>
                     <Card.Subtitle className="mb-2 feed-text">{props.content}</Card.Subtitle>
                     <Card.Subtitle className="mb-2 feed-text">{`Tags: ${props.tags}`}</Card.Subtitle>
@@ -28,10 +30,13 @@ export default function Post(props) {
                             <i className='bi bi-hand-thumbs-up-fill text-primary unclickable' style={postStyles[0]}></i>
                             <p className={`${props.likeButtonBefore} unclickable`}>{props.likes}</p>
                         </button>
-                        <button className='btn btn-primary feed-button-two' onClick={props.handleShares}>
-                            <i className='bi bi-share-fill unclickable' style={postStyles[0]}></i>
-                            <p className={`${props.shareButtonBefore} unclickable`}>{props.shares}</p>
-                        </button>
+                        <Tippy content='Only one share allowed!!!' className="tippy-tooltip">
+                            <button className='btn btn-primary feed-button-two' onClick={props.handleShares}>
+                                <i className='bi bi-share-fill unclickable' style={postStyles[0]}> </i>
+                                <p className={`${props.shareButtonBefore} unclickable`}>{props.shares}</p>
+                            </button>
+                        </Tippy>
+
                     </div>
                 </Card.Body>
             </Card>
